4903e309b9257ab2876465fa1c4925c94445b043
ankitdmon <ankitmishra2108@gmail.com>
2022-11-24 00:02:35 +0530

    Theme setting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+
+
 function App() {
+
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app"> app </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
